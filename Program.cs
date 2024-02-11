4de83ca61c7f1cea877ce6fb55e36597b144089a
Ariane Emory <ariane.emory@protonmail.com>
2024-02-10 21:43:38 -0500

    ...

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ class Program
 {
    static void Main()
    {
-      var tokens = new Ae.Tokenizer().Tokenize(
+      var tokens = new Tokenizer().Tokenize(
         @"(123 457 *bingo* <boop> nil (789 nil)) 12 34 (1 . (2 3)) 'nil '123 "
         + @"`(1 ,2 3) $(1 2 3) (1 2 ,@(4 5)) ?a 'b' '\n' ?\n ""he\""llo"" "
         + @" ""one \""two\"" three"" -05 1,234 2,3,4 1/3 -3/5 -4/1,200 ??? "
@@ -20,34 +20,34 @@ class Program
 
       // Print out the tokenized tokens:
       foreach (var (token, index) in tokens
-         .Where(token => token.TokenType != Ae.TokenType.Whitespace)
+         .Where(token => token.TokenType != TokenType.Whitespace)
          .Select((value, index) => (value, index)))
          Console.WriteLine($"#{index}: {token.TokenType} [{token.Text}]");
 
       var lastToken = tokens.ToList()[tokens.Count()-1];
 
       // Die if we didn't tokenize everything:
-      // if (lastToken.TokenType == Ae.TokenType.Garbage)
+      // if (lastToken.TokenType == TokenType.Garbage)
       //    Die(1, $"Failed to tokenize the entire input, remaining text: \"{lastToken.Text}\"");
 
-      Ae.Pair properList = Ae.Cons(new Ae.Symbol("one"), Ae.Cons(new Ae.Symbol("two"), Ae.Cons(new Ae.Symbol("three"), Ae.Nil)));
-      Ae.Pair improperList = Ae.Cons(new Ae.Symbol("one"), Ae.Cons(new Ae.Integer(37), Ae.Cons(new Ae.Rational(3, 4), new Ae.Symbol("four"))));
+      Pair properList = Cons(new Symbol("one"), Cons(new Symbol("two"), Cons(new Symbol("three"), Nil)));
+      Pair improperList = Cons(new Symbol("one"), Cons(new Integer(37), Cons(new Rational(3, 4), new Symbol("four"))));
 
       Console.WriteLine(properList);
-      Console.WriteLine(Ae.Write(properList));
+      Console.WriteLine(Write(properList));
 
       foreach (var obj in properList)
         Console.WriteLine(obj);
 
       Console.WriteLine(improperList);
-      Console.WriteLine(Ae.Write(improperList));
+      Console.WriteLine(Write(improperList));
 
       foreach (var obj in improperList)
         Console.WriteLine(obj);
 
-      Console.WriteLine(Ae.Write(new Ae.Lambda(Ae.Nil, Ae.Nil, Ae.Nil)));
-      Console.WriteLine(Ae.Write(new Ae.Lambda(Ae.Nil, Ae.Nil, Ae.Nil)));
-      Console.WriteLine(Ae.Write(new Ae.Lambda(Ae.Nil, Ae.Nil, Ae.Nil)));
+      Console.WriteLine(Write(new Lambda(Nil, Nil, Nil)));
+      Console.WriteLine(Write(new Lambda(Nil, Nil, Nil)));
+      Console.WriteLine(Write(new Lambda(Nil, Nil, Nil)));
 
       Console.WriteLine("Done.");
    }
