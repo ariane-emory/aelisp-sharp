26e067817c56175b7ae6c740847816496d390586
Ariane Emory <ariane.emory@protonmail.com>
2024-02-12 12:31:20 -0500

    ...

--- Program.cs
+++ Program.cs
@@ -2,43 +2,49 @@
 using static Ae;
 using static System.Console;
 
+enum Mode
+{
+  Line,
+  File,
+};
+
 class Program
 {
   static void Main()
   {
     var filename = "data.lisp";
+    var totalTokens = 0;
 
-    for (int ix = 0; ix < 3; ix++)
+    Tokenizer.Get().Reset();
+
+    if (!File.Exists(filename))
+      throw new ApplicationException($"No file '{filename}'.");
+
+    var tokens = new List<PositionedToken<TokenType>>();
+    var input = File.ReadAllLines(filename);
+
+    foreach (var (line, lineNumber) in input.Select((line, lineNumber) => (line, lineNumber)))
     {
-      Tokenizer.Get().Reset();
+      var newTokens = Tokenizer.Get().Tokenize($"{line}", false)
+        .Select(t => new PositionedToken<TokenType>(t.TokenType, t.Text, lineNumber, t.Column)).ToList();
 
-      if (!File.Exists(filename))
-        throw new ApplicationException($"No file '{filename}'.");
+      totalTokens += newTokens.Count;
 
-      var tokens = new List<PositionedToken<TokenType>>();
-      var input = File.ReadAllLines(filename);
-
-      foreach (var (line, lineNumber) in input.Select((line, lineNumber) => (line, lineNumber)))
+      if (newTokens.Any())
       {
-        var newTokens = Tokenizer.Get().Tokenize($"{line}", false)
-          .Select(t => new PositionedToken<TokenType>(t.TokenType, t.Text, lineNumber, t.Column)).ToList();
+        var lastNewToken = newTokens.ToList()[Math.Max(0, newTokens.Count() - 1)];
 
-        if (newTokens.Any())
-        {
-          var lastNewToken = newTokens.ToList()[Math.Max(0, newTokens.Count() - 1)];
-
-          if (lastNewToken.TokenType == TokenType.Garbage)
-            Die(1, $"Failed to tokenize the entire input, remaining text: \"{lastNewToken.Text}\"");  
-        }
-
-        // Print out the tokenized tokens:
-        foreach (var (token, index) in newTokens
-         .Where(token => token.TokenType != TokenType.Whitespace)
-         .Select((value, index) => (value, index)))
-          WriteLine($"#{index}: {token}");
-
-        tokens.AddRange(newTokens);
+        if (lastNewToken.TokenType == TokenType.Garbage)
+          Die(1, $"Failed to tokenize the entire input, remaining text: \"{lastNewToken.Text}\"");
       }
+
+      // Print out the tokenized tokens:
+      foreach (var (token, index) in newTokens
+       .Where(token => token.TokenType != TokenType.Whitespace)
+       .Select((value, index) => (value, index)))
+        WriteLine($"#{index}: {token}");
+
+      tokens.AddRange(newTokens);
 
       // Die if we tokenized nothing:
       if (!tokens.Any())
@@ -49,11 +55,9 @@ class Program
       // Die if we didn't tokenize everything:
       if (lastToken.TokenType == TokenType.Garbage)
         Die(1, $"Failed to tokenize the entire input, remaining text: \"{lastToken.Text}\"");
-
-      WriteLine("\n");
     }
 
-    Die(0, "Tokenized all input.");
+    Die(0, $"Tokenized all input, {totalTokens} tokens.");
 
     Pair properList = Cons(new Symbol("one"), Cons(new Symbol("two"), Cons(new Symbol("three"), Nil)));
     Pair improperList = Cons(new Symbol("one"), Cons(new Integer(37), Cons(new Rational(3, 4), new Symbol("four"))));
