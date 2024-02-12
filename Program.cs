0de041ef742fcd12371ee3ab9acc00fb1c88be5c
Ariane Emory <ariane.emory@protonmail.com>
2024-02-11 22:09:16 -0500

    ...

--- Program.cs
+++ Program.cs
@@ -6,21 +6,21 @@ class Program
 {
   static void Main()
    {
-     var tokens = Tokenizer.Get().Tokenize(@"one two");
+     var tokens = Tokenizer.Get().Tokenize( // @"one two");
 
-//         @"one two
-// three (123 457 *bingo* <boop> nil (789 nil)) 12 34 (1 . (2 3)) 'nil '123 "
-//         + @"`(1 ,2 3) $(1 2 3) (1 2 ,@(4 5)) ?a ?\n ""he\""llo"" "
-//         + @" ""one \""two\"" three"" -05 1,234 2,3,4 1/3 -3/5 -4/1,200 ??? "
-//         + @"here--it/is! %another-symbol1-23//asd 1+ 10- ∨ ⊤ ⊥ ≤ ≥ × 12.004 "
-//         + @"-.24 -0.43 *bingo* bang!! <Bob'sAge> 1,024.4 a.b_c.2
-//  pi_is_3.14 "
-//         + @">> <= 1- 2- -2 - %2 2% 02,345 1,234 -1,234 >>2 <<3 2>> 3<< << >> "
-//         + @"a::b/c :keyword @
-// &rest -a / !abc ! <!good> 'a' '\n' '\""' "
-//         + @" ""hello\nworld"" "
-//         + @" ?a ?\n ");
-// //        + @"\#!2garbage");
+        @"one two
+three (123 457 *bingo* <boop> nil (789 nil)) 12 34 (1 . (2 3)) 'nil '123 "
+        + @"`(1 ,2 3) $(1 2 3) (1 2 ,@(4 5)) ?a ?\n ""he\""llo"" "
+        + @" ""one \""two\"" three"" -05 1,234 2,3,4 1/3 -3/5 -4/1,200 ??? "
+        + @"here--it/is! %another-symbol1-23//asd 1+ 10- ∨ ⊤ ⊥ ≤ ≥ × 12.004 "
+        + @"-.24 -0.43 *bingo* bang!! <Bob'sAge> 1,024.4 a.b_c.2
+ pi_is_3.14 "
+        + @">> <= 1- 2- -2 - %2 2% 02,345 1,234 -1,234 >>2 <<3 2>> 3<< << >> "
+        + @"a::b/c :keyword @
+&rest -a / !abc ! <!good> 'a' '\n' '\""' "
+        + @" ""hello\nworld"" "
+        + @" ?a ?\n ");
+//        + @"\#!2garbage");
 
       // Die if we tokenized nothing:
       if (! tokens.Any())
