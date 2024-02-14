77eebb6ec528bcd2fad945a049ca1beda48b19a4
Ariane Emory <ariane.emory@protonmail.com>
2024-02-13 21:36:11 -0500

    ...

--- src/AeLispTokenizer.cs
+++ src/AeLispTokenizer.cs
@@ -120,7 +120,7 @@ public static partial class Ae
       .Add((Type: TokenType.String,                     Discrete: true,  Process: null,              IsActive: NotInMultilineString, Pattern: @"\""" + StringContent+ @"\"""))
 
       .Add((Type: TokenType.MultilineStringBeginning,   Discrete: false, Process: BeginMLS,          IsActive: NotInMultilineString, Pattern: @"\""" + StringContent+ @"\n"))
-      .Add((Type: TokenType.MultilineStringEnd,         Discrete: true,  Process: EndMLS,            IsActive: InMultilineString,    Pattern: @"(?:(?!\\"")[^\n])*\"""))
+      .Add((Type: TokenType.MultilineStringEnd,         Discrete: true,  Process: EndMLS,            IsActive: InMultilineString,    Pattern: StringContent + @"\"""))
       .Add((Type: TokenType.MultilineStringContent,     Discrete: false, Process: null,              IsActive: InMultilineString,    Pattern: @"(?:(?!\\"")[^\n])*\n"))
 
       .Add((Type: TokenType.InlineComment,              Discrete: false, Process: null,              IsActive: null,                 Pattern: @"#\|[^\n]*\|#"))
