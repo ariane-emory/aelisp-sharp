7db36010c9727b0560fb7055692006380083c1a2
Ariane Emory <ariane.emory@protonmail.com>
2024-02-15 02:05:57 -0500

    ...

--- Ae/Utility.cs
+++ Ae/Utility.cs
@@ -1,8 +1,13 @@
-using System.Text.RegularExpressions;
-
 //======================================================================================================================
 static partial class Ae
 {
+  public static bool EnableDebugWrite { get; set; } = false;
+  
+  public static void DebugWrite(string s) {
+    if (EnableDebugWrite)
+      Console.WriteLine(s);
+  }
+  
   public static void Die(int exitCode, string message)
     {
     Console.Error.WriteLine(message);
@@ -10,6 +15,6 @@ static partial class Ae
   }
 
   public static string ReplaceNewlinesWithEscaped(this string input) => input.Replace("\n", "\\n");
-  
+
   //====================================================================================================================
 }
