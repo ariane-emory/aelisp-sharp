bd887c16b4e56177f7152ba45671e55e53c01629
Ariane Emory <ariane.emory@protonmail.com>
2024-02-16 02:10:23 -0500

    ...

--- Ae/TokenStream.cs
+++ Ae/TokenStream.cs
@@ -119,7 +119,7 @@ static partial class Ae
         DebugWrite($"Stream.Next:                Return no token!");
 
         throw new ApplicationException($"Encountered bad input on line {_state.Value.Line}, "
-                                       + $"column {_state.Value.Column} at \"{_input}\"!");
+                                       + $"column {_state.Value.Column} at \"" + _input + "\"!");
       }
       else
       {
