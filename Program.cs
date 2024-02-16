06b863e57096795298af55dc7d436f4f64034b37
Ariane Emory <ariane.emory@protonmail.com>
2024-02-16 00:35:29 -0500

    ...

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ using static Pidgin.Parser<Ae.Token>;
 class Program
 {
   //====================================================================================================================
-  static Parser<Token, Token> TokenWithTokenType(TokenType tokenType) => Parser<Token>.Token(t => t.TokenType == tokenType);
+  static Parser<Token, Token> TokenWithType(TokenType tokenType) => Parser<Token>.Token(t => t.TokenType == tokenType);
 
   //====================================================================================================================
   static readonly ImmutableArray<TokenType> MultilineCommentTokenTypes =
@@ -19,16 +19,16 @@ class Program
   static readonly ImmutableArray<TokenType> MultilineStringTokenTypes =
     ImmutableArray.Create(TokenType.MultilineStringBegin, TokenType.MultilineStringContent, TokenType.MultilineStringEnd);
 
-  static readonly Parser<Token, Token> MultilineCommentBegin = TokenWithTokenType(TokenType.MultilineCommentBegin);
-  static readonly Parser<Token, Token> MultilineCommentContent = TokenWithTokenType(TokenType.MultilineCommentContent);
-  static readonly Parser<Token, Token> MultilineCommentEnd = TokenWithTokenType(TokenType.MultilineCommentEnd);
-  static readonly Parser<Token, Token> MultilineStringBegin = TokenWithTokenType(TokenType.MultilineStringBegin);
-  static readonly Parser<Token, Token> MultilineStringContent = TokenWithTokenType(TokenType.MultilineStringContent);
-  static readonly Parser<Token, Token> MultilineStringEnd = TokenWithTokenType(TokenType.MultilineStringEnd);
-  static readonly Parser<Token, Token> SomeOtherToken = Token(t => (!MultilineCommentTokenTypes.Contains(t.TokenType))
+  static readonly Parser<Token, Token> MultilineCommentBegin = TokenWithType(TokenType.MultilineCommentBegin);
+  static readonly Parser<Token, Token> MultilineCommentContent = TokenWithType(TokenType.MultilineCommentContent);
+  static readonly Parser<Token, Token> MultilineCommentEnd = TokenWithType(TokenType.MultilineCommentEnd);
+  static readonly Parser<Token, Token> MultilineStringBegin = TokenWithType(TokenType.MultilineStringBegin);
+  static readonly Parser<Token, Token> MultilineStringContent = TokenWithType(TokenType.MultilineStringContent);
+  static readonly Parser<Token, Token> MultilineStringEnd = TokenWithType(TokenType.MultilineStringEnd);
+  static readonly Parser<Token, Token> NotAMultilineToken = Token(t => (!MultilineCommentTokenTypes.Contains(t.TokenType))
                                                               && (!MultilineStringTokenTypes.Contains(t.TokenType)));
 
-  static readonly Parser<Token, Token> ConsolidateMultilineComment =
+  static readonly Parser<Token, Token> MergeMultilineComment =
     from begin in MultilineCommentBegin
     from contents in MultilineCommentContent.Many()
     from end in MultilineCommentEnd
@@ -39,7 +39,7 @@ class Program
       begin.Column
       );
 
-  static readonly Parser<Token, Token> ConsolidateMultilineString =
+  static readonly Parser<Token, Token> MergeMultilineString =
     from begin in MultilineStringBegin
     from contents in MultilineStringContent.Many()
     from end in MultilineStringEnd
@@ -50,9 +50,9 @@ class Program
       begin.Column
       );
 
-  static readonly Parser<Token, IEnumerable<Token>> ConsolidateMultilineTokens =
-    OneOf(SomeOtherToken, ConsolidateMultilineComment, ConsolidateMultilineString).Many();
-  
+  static readonly Parser<Token, IEnumerable<Token>> MergeMultilineTokens =
+    OneOf(NotAMultilineToken, MergeMultilineComment, MergeMultilineString).Many();
+
   //====================================================================================================================
   static void Main()
   {
@@ -66,7 +66,7 @@ class Program
     WriteLine("\nBefore parse: ");
     tokens.Print(); // tokens are printed correctly..
 
-    var result = ConsolidateMultilineTokens.ParseOrThrow(tokens);
+    var result = MergeMultilineTokens.ParseOrThrow(tokens);
 
     WriteLine("\nResult of parse: ");
     result.Print();
