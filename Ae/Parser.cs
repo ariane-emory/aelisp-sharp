627a6abbd1f434e7f5db25502f927b8262d9fd26
Ariane Emory <ariane.emory@protonmail.com>
2024-02-16 01:29:14 -0500

    ...

--- Ae/Parser.cs
+++ Ae/Parser.cs
@@ -16,12 +16,13 @@ static partial class Ae
     //======================================================================================================================================
     private static Parser<Token, Token> TokenWithType(TokenType tokenType) => Parser<Token>.Token(t => t.Type == tokenType);
 
-    private static Parser<Token, Token> MergeMultilineSequence(TokenType beginToken, TokenType contentToken, TokenType endToken) =>
+    private static Parser<Token, Token>
+    MergeMultilineSequence(TokenType outToken, TokenType beginToken, TokenType contentToken, TokenType endToken) =>
       from begin in TokenWithType(beginToken)
       from contents in TokenWithType(contentToken).Many()
       from end in TokenWithType(endToken)
       select new Token(
-        TokenType.Comment,
+        outToken,
         string.Join("", new[] { begin }.Concat(contents).Append(end).Select(t => t.Text)),
         begin.Line,
         begin.Column
@@ -42,8 +43,8 @@ static partial class Ae
     public static readonly Parser<Token, IEnumerable<Token>> MergeMultilineTokens =
       OneOf(Token(t => (!MultilineCommentTokenTypes.Contains(t.Type))
                   && (!MultilineStringTokenTypes.Contains(t.Type))),
-            MergeMultilineSequence(TokenType.MultilineCommentBegin, TokenType.MultilineCommentContent, TokenType.MultilineCommentEnd),
-            MergeMultilineSequence(TokenType.MultilineStringBegin, TokenType.MultilineStringContent, TokenType.MultilineStringEnd))
+            MergeMultilineSequence(TokenType.Comment, TokenType.MultilineCommentBegin, TokenType.MultilineCommentContent, TokenType.MultilineCommentEnd),
+            MergeMultilineSequence(TokenType.String, TokenType.MultilineStringBegin, TokenType.MultilineStringContent, TokenType.MultilineStringEnd))
       .Many();
     //======================================================================================================================================
     }
