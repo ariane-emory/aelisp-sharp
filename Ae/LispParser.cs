a6eb721fa8696331028a5fdfa9da7a25ebb18576
Ariane Emory <ariane.emory@protonmail.com>
2024-02-16 20:34:56 -0500

    ...

--- Ae/LispParser.cs
+++ Ae/LispParser.cs
@@ -92,6 +92,12 @@ static partial class Ae
         ParseLispStyleChar
         );
 
+    private static readonly Parser<LispToken, LispObject> ParseList =
+      TypedToken(LispTokenType.LParen)
+      .Then(ParseListElements!)
+      .Before(TypedToken(LispTokenType.RParen));
+
+
     //======================================================================================================================================
     // Public Parsers
     //======================================================================================================================================
@@ -142,11 +148,6 @@ static partial class Ae
             }
         )
     );
-
-    private static readonly Parser<LispToken, LispObject> ParseList =
-      TypedToken(LispTokenType.LParen)
-      .Then(ParseListElements)
-      .Before(TypedToken(LispTokenType.RParen));
 
     private static readonly Parser<LispToken, LispObject> QuotedSExp =
       TypedToken(LispTokenType.Quote)
