aa55bd3e6607565b3b2980950a55c06e6527d745
Ariane Emory <ariane.emory@protonmail.com>
2024-02-17 02:15:14 -0500

    ...

---
+++ Ae/Pair.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Reflection;
+using System.Text;
+using static Ae;
+
+//======================================================================================================================
+static partial class Ae
+{
+   //====================================================================================================================
+   // Pair class
+   //====================================================================================================================
+   public class Pair : LispObject, IEnumerable<LispObject>
+   {
+      //==================================================================================================================
+      // Public properties
+      //==================================================================================================================
+      public LispObject Car { get; }
+      public LispObject Cdr { get; }
+
+      //==================================================================================================================
+      // Constructor
+      //==================================================================================================================
+      public Pair(LispObject car, LispObject cdr)
+      {
+         Car = car;
+         Cdr = cdr;
+      }
+
+      //==================================================================================================================
+      // Instance methods
+      //==================================================================================================================
+      public override string ToString() => $"{TypeName}({Car}, {Cdr})";
+
+      //==================================================================================================================
+      public override string Write()
+      {
+         var sb = new StringBuilder();
+
+         sb.Append("(");
+
+         LispObject current = this;
+
+         while (current is Pair currentCons)
+         {
+            sb.Append(currentCons.Car.Write());
+
+            if (currentCons.Cdr is Pair)
+            {
+               sb.Append(" ");
+               current = currentCons.Cdr;
+            }
+            else if (currentCons.Cdr != Nil)
+            {
+
+               sb.Append(" . ");
+               sb.Append(currentCons.Cdr.Write());
+
+               break;
+            }
+            else
+            {
+               break;
+            }
+         }
+
+         sb.Append(")");
+
+         return sb.ToString();
+      }
+
+      //==================================================================================================================
+      public IEnumerator<LispObject> GetEnumerator()
+      {
+         LispObject current = this;
+
+         while (current != Nil)
+            if (current is Pair pair)
+            {
+               yield return pair.Car;
+
+               current = pair.Cdr;
+            }
+            else
+            {
+               yield return current;
+               yield break;
+
+               // throw new InvalidOperationException("Enumerated improper list!");
+            }
+      }
+
+      //==================================================================================================================
+      IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+   }
+
+   //====================================================================================================================
+}
