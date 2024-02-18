41af76a542ddd3c07758bcd55816f2798b730037
Ariane Emory <ariane.emory@protonmail.com>
2024-02-18 07:43:44 -0500

    ...

--- aelisp/Program.cs
+++ aelisp/Program.cs
@@ -15,76 +15,79 @@ class Program
    //==============================================================================================================================
    static void Main()
    {
-      // EnableDebugWrite = true;
-
-      var path = "data/data.lisp"; // "~/lib.lisp";
-      var expandedPath = path.ExpandTilde();
-      var fileText = File.ReadAllText(expandedPath);
-
-      // WriteLine("File contents:");
-      // WriteLine(fileText);
-      // WriteLine("EOF");
-
-      var tokenizer = new QueueingLispTokenizer(fileText, exclude: IsWhitespaceToken);
-
-      // WriteLine("\nSTEP 1 - Raw tokens: ");
-      IEnumerable<LispToken> tokens = tokenizer.ReadAll();
-      // tokens.Print();
-
-      // WriteLine("\nSTEP 2 - Tokens after merging multiline tokens: ");
-      tokens = MergeMultilineTokens.ParseOrThrow(tokens);
-      //tokens.Print();
-
-      // WriteLine("\nSTEP 3 - Tokens after excluding comments: ");
-      tokens = tokens.ExcludingComments();
-      // tokens.Print();
-
-      WriteLine("\nSTEP 4 - Parsed objects: ");
-      try
+      if (false)
       {
-         var obj = ParseProgram.ParseOrThrow(tokens);
+         // EnableDebugWrite = true;
 
-         // WriteLine($"obj.toString(): {obj}");
-         // WriteLine($"obj.Princ(): {obj.Princ()}");
+         var path = "data/data.lisp"; // "~/lib.lisp";
+         var expandedPath = path.ExpandTilde();
+         var fileText = File.ReadAllText(expandedPath);
 
-         // if (obj is Pair pair)
-         //    foreach (var o in pair)
-         //       WriteLine(o.Princ());
-         // else
-         WriteLine(obj.Princ());
-      }
-      catch (ParseException e)
-      {
-         var re = new Regex(@"unexpected[\s\S]*at line \d+, col (\d+)", RegexOptions.Multiline);
-         var match = re.Match(e.Message);
+         // WriteLine("File contents:");
+         // WriteLine(fileText);
+         // WriteLine("EOF");
 
-         if (match.Success)
+         var tokenizer = new QueueingLispTokenizer(fileText, exclude: IsWhitespaceToken);
+
+         // WriteLine("\nSTEP 1 - Raw tokens: ");
+         IEnumerable<LispToken> tokens = tokenizer.ReadAll();
+         // tokens.Print();
+
+         // WriteLine("\nSTEP 2 - Tokens after merging multiline tokens: ");
+         tokens = MergeMultilineTokens.ParseOrThrow(tokens);
+         //tokens.Print();
+
+         // WriteLine("\nSTEP 3 - Tokens after excluding comments: ");
+         tokens = tokens.ExcludingComments();
+         // tokens.Print();
+
+         WriteLine("\nSTEP 4 - Parsed objects: ");
+         try
          {
-            // ParseException reports 1-based index, convert it to 0-based
-            var ix = int.Parse(match.Groups[1].Value) - 1;
+            var obj = ParseProgram.ParseOrThrow(tokens);
 
-            if (ix >= 0 && ix < tokens.Count())
+            // WriteLine($"obj.toString(): {obj}");
+            // WriteLine($"obj.Princ(): {obj.Princ()}");
+
+            // if (obj is Pair pair)
+            //    foreach (var o in pair)
+            //       WriteLine(o.Princ());
+            // else
+            WriteLine(obj.Princ());
+         }
+         catch (ParseException e)
+         {
+            var re = new Regex(@"unexpected[\s\S]*at line \d+, col (\d+)", RegexOptions.Multiline);
+            var match = re.Match(e.Message);
+
+            if (match.Success)
             {
-               var tok = tokens.ElementAt(ix);
-               WriteLine($"ERROR: Unexpected token at line {tok.Line + 1}, column {tok.Column}: {tok}.");
+               // ParseException reports 1-based index, convert it to 0-based
+               var ix = int.Parse(match.Groups[1].Value) - 1;
+
+               if (ix >= 0 && ix < tokens.Count())
+               {
+                  var tok = tokens.ElementAt(ix);
+                  WriteLine($"ERROR: Unexpected token at line {tok.Line + 1}, column {tok.Column}: {tok}.");
+               }
+               else
+               {
+                  WriteLine($"ERROR: Error at a position that could not be directly mapped to a token: {e.Message}");
+               }
             }
             else
             {
-               WriteLine($"ERROR: Error at a position that could not be directly mapped to a token: {e.Message}");
+               WriteLine($"ERROR: Parse error: {e.Message}");
             }
-         }
-         else
-         {
-            WriteLine($"ERROR: Parse error: {e.Message}");
-         }
 
-         Die(2, "Dying due to parse error.");
+            Die(2, "Dying due to parse error.");
+         }
       }
 
       var root_env = new Env(
-         Nil,
-         Cons(Intern("x"), Cons(Intern("y"), Cons(Intern("z"), Nil))),
-         Cons(new Integer(1), Cons(new Integer(2), Cons(new Integer(3), Nil))));
+       Nil,
+       Cons(Intern("x"), Cons(Intern("y"), Cons(Intern("z"), Nil))),
+       Cons(new Integer(1), Cons(new Integer(2), Cons(new Integer(3), Nil))));
 
       var parent_env = new Env(
          root_env,
@@ -96,169 +99,172 @@ class Program
          Cons(Intern("xxx"), Cons(Intern("yyy"), Cons(Intern("zzz"), Nil))),
          Cons(new Integer(111), Cons(new Integer(222), Cons(new Integer(333), Nil))));
 
-      WriteLine(child_env.Princ());
+      if (false)
+      {
+         WriteLine(child_env.Princ());
 
-      var (found, fobj) = child_env.Lookup(Env.LookupMode.Nearest, Intern("x"));
-      WriteLine($"found: {found}, fobj: {fobj}");
+         var (found1, fobj1) = child_env.Lookup(Env.LookupMode.Nearest, Intern("x"));
+         WriteLine($"found1: {found1}, fobj1: {fobj1}");
 
-      (found, fobj) = child_env.Lookup(Env.LookupMode.Nearest, Intern("y"));
-      WriteLine($"found: {found}, fobj: {fobj}");
+         (found1, fobj1) = child_env.Lookup(Env.LookupMode.Nearest, Intern("y"));
+         WriteLine($"found1: {found1}, fobj1: {fobj1}");
 
-      (found, fobj) = child_env.Lookup(Env.LookupMode.Nearest, Intern("z"));
-      WriteLine($"found: {found}, fobj: {fobj}");
+         (found1, fobj1) = child_env.Lookup(Env.LookupMode.Nearest, Intern("z"));
+         WriteLine($"found1: {found1}, fobj1: {fobj1}");
 
-      (found, fobj) = child_env.Lookup(Env.LookupMode.Nearest, Intern("xx"));
-      WriteLine($"found: {found}, fobj: {fobj}");
+         (found1, fobj1) = child_env.Lookup(Env.LookupMode.Nearest, Intern("xx"));
+         WriteLine($"found1: {found1}, fobj1: {fobj1}");
 
-      (found, fobj) = child_env.Lookup(Env.LookupMode.Nearest, Intern("nil"));
-      WriteLine($"found: {found}, fobj: {fobj}");
+         (found1, fobj1) = child_env.Lookup(Env.LookupMode.Nearest, Intern("nil"));
+         WriteLine($"found1: {found1}, fobj1: {fobj1}");
 
-      (found, fobj) = child_env.Lookup(Env.LookupMode.Nearest, Intern("t"));
-      WriteLine($"found: {found}, fobj: {fobj}");
+         (found1, fobj1) = child_env.Lookup(Env.LookupMode.Nearest, Intern("t"));
+         WriteLine($"found1: {found1}, fobj1: {fobj1}");
 
-      (found, fobj) = child_env.Lookup(Env.LookupMode.Nearest, Intern("yyy"));
-      WriteLine($"found: {found}, fobj: {fobj}");
+         (found1, fobj1) = child_env.Lookup(Env.LookupMode.Nearest, Intern("yyy"));
+         WriteLine($"found1: {found1}, fobj1: {fobj1}");
 
-      WriteLine("");
-      WriteLine($"root:   {root_env}");
-      WriteLine($"parent: {parent_env}");
-      WriteLine($"child:  {child_env}");
+         WriteLine("");
+         WriteLine($"root:   {root_env}");
+         WriteLine($"parent: {parent_env}");
+         WriteLine($"child:  {child_env}");
 
-      child_env.Set(Env.LookupMode.Global, Intern("xx"), new Integer(9999));
+         child_env.Set(Env.LookupMode.Global, Intern("xx"), new Integer(9999));
 
-      WriteLine("");
-      WriteLine($"root:   {root_env}");
-      WriteLine($"parent: {parent_env}");
-      WriteLine($"child:  {child_env}");
+         WriteLine("");
+         WriteLine($"root:   {root_env}");
+         WriteLine($"parent: {parent_env}");
+         WriteLine($"child:  {child_env}");
 
-      child_env.Set(Env.LookupMode.Nearest, Intern("xx"), new Integer(3333));
+         child_env.Set(Env.LookupMode.Nearest, Intern("xx"), new Integer(3333));
 
-      WriteLine("");
-      WriteLine($"root:   {root_env}");
-      WriteLine($"parent: {parent_env}");
-      WriteLine($"child:  {child_env}");
+         WriteLine("");
+         WriteLine($"root:   {root_env}");
+         WriteLine($"parent: {parent_env}");
+         WriteLine($"child:  {child_env}");
 
-      child_env.Set(Env.LookupMode.Local, Intern("xx"), new Integer(6666));
+         child_env.Set(Env.LookupMode.Local, Intern("xx"), new Integer(6666));
 
-      WriteLine("");
-      WriteLine($"root:   {root_env}");
-      WriteLine($"parent: {parent_env}");
-      WriteLine($"child:  {child_env}");
+         WriteLine("");
+         WriteLine($"root:   {root_env}");
+         WriteLine($"parent: {parent_env}");
+         WriteLine($"child:  {child_env}");
 
-      child_env.Set(Env.LookupMode.Local, Intern("xxyyxx"), new Integer(6666));
+         child_env.Set(Env.LookupMode.Local, Intern("xxyyxx"), new Integer(6666));
 
-      WriteLine("");
-      WriteLine($"root:   {root_env}");
-      WriteLine($"parent: {parent_env}");
-      WriteLine($"child:  {child_env}");
+         WriteLine("");
+         WriteLine($"root:   {root_env}");
+         WriteLine($"parent: {parent_env}");
+         WriteLine($"child:  {child_env}");
 
-      child_env.Set(Env.LookupMode.Local, Intern("yyxxyy"), new Integer(6666));
+         child_env.Set(Env.LookupMode.Local, Intern("yyxxyy"), new Integer(6666));
 
-      WriteLine("");
-      WriteLine($"root:   {root_env}");
-      WriteLine($"parent: {parent_env}");
-      WriteLine($"child:  {child_env}");
+         WriteLine("");
+         WriteLine($"root:   {root_env}");
+         WriteLine($"parent: {parent_env}");
+         WriteLine($"child:  {child_env}");
 
-      child_env.Set(Env.LookupMode.Global, Intern("yyxxyy"), new Integer(6666));
+         child_env.Set(Env.LookupMode.Global, Intern("yyxxyy"), new Integer(6666));
 
-      WriteLine("");
-      WriteLine($"root:   {root_env}");
-      WriteLine($"parent: {parent_env}");
-      WriteLine($"child:  {child_env}");
+         WriteLine("");
+         WriteLine($"root:   {root_env}");
+         WriteLine($"parent: {parent_env}");
+         WriteLine($"child:  {child_env}");
 
-      Symbol sym1 = (Symbol)Intern("xx");
-      LispObject sym2 = Intern("xx");
-      var (found1, obj1) = child_env.Lookup(Env.LookupMode.Nearest, sym1);
-      var (found2, obj2) = child_env.Lookup(Env.LookupMode.Nearest, sym2);
+         Symbol sym1 = (Symbol)Intern("xx");
+         LispObject sym2 = Intern("xx");
+         var (found11, obj1) = child_env.Lookup(Env.LookupMode.Nearest, sym1);
+         var (found12, obj2) = child_env.Lookup(Env.LookupMode.Nearest, sym2);
 
-      WriteLine(obj1);
-      WriteLine(obj2);
+         WriteLine(obj1);
+         WriteLine(obj2);
 
-      WriteLine("\nStop.");
+         WriteLine("\nStop.");
 
-      var proper = (Pair)Cons(new Integer(0), Cons(new Integer(1), Cons(new Integer(2), Cons(new Integer(3), Nil))));
-      var improper = (Pair)Cons(new Integer(0), Cons(new Integer(1), Cons(new Integer(2), new Integer(3))));
+         var proper = (Pair)Cons(new Integer(0), Cons(new Integer(1), Cons(new Integer(2), Cons(new Integer(3), Nil))));
+         var improper = (Pair)Cons(new Integer(0), Cons(new Integer(1), Cons(new Integer(2), new Integer(3))));
 
-      int len = 0;
-      foreach (var o in proper)
-         len++;
+         int len = 0;
+         foreach (var o in proper)
+            len++;
 
-      int len2 = 0;
-      foreach (var o in improper)
-         len2++;
+         int len2 = 0;
+         foreach (var o in improper)
+            len2++;
 
-      WriteLine(len);
-      WriteLine(len2);
-      WriteLine(proper.Length);
-      WriteLine(improper.Length);
+         WriteLine(len);
+         WriteLine(len2);
+         WriteLine(proper.Length);
+         WriteLine(improper.Length);
 
-      WriteLine(Intern("xx").Eval(child_env));
-      WriteLine(Intern(":xx").Eval(child_env));
-      WriteLine(Intern("nil").Eval(child_env));
-      WriteLine(Nil.Eval(child_env));
+         WriteLine(Intern("xx").Eval(child_env));
+         WriteLine(Intern(":xx").Eval(child_env));
+         WriteLine(Intern("nil").Eval(child_env));
+         WriteLine(Nil.Eval(child_env));
 
-      LispObject lst = Cons(new Integer(11), Cons(new Char('a'), Cons(Intern("bang"), Cons(Intern(":asd"), Cons(new String("hello"), Nil)))));
-      WriteLine(lst.Princ());
-      WriteLine(lst.Print());
-      LispObject str = new String("hello");
-      WriteLine(str.Princ());
-      WriteLine(str.Print());
-      LispObject chr = new Char('x');
-      WriteLine(chr.Princ());
-      WriteLine(chr.Print());
+         LispObject lst = Cons(new Integer(11), Cons(new Char('a'), Cons(Intern("bang"), Cons(Intern(":asd"), Cons(new String("hello"), Nil)))));
+         WriteLine(lst.Princ());
+         WriteLine(lst.Print());
+         LispObject str = new String("hello");
+         WriteLine(str.Princ());
+         WriteLine(str.Print());
+         LispObject chr = new Char('x');
+         WriteLine(chr.Princ());
+         WriteLine(chr.Print());
 
-      WriteLine("");
-      WriteLine(improper[0].Princ());
-      WriteLine(improper[1].Princ());
-      WriteLine(improper[2].Princ());
-      WriteLine(improper[3].Princ());
-      WriteLine(improper[4].Princ());
-      WriteLine(improper[5].Princ());
+         WriteLine("");
+         WriteLine(improper[0].Princ());
+         WriteLine(improper[1].Princ());
+         WriteLine(improper[2].Princ());
+         WriteLine(improper[3].Princ());
+         WriteLine(improper[4].Princ());
+         WriteLine(improper[5].Princ());
 
-      improper[0] = new Integer(10);
-      improper[1] = new Integer(11);
-      improper[2] = new Integer(12);
-      improper[3] = new Integer(13);
-      // improper[4] = new Integer(14);
-      // improper[4] = new Integer(15);
+         improper[0] = new Integer(10);
+         improper[1] = new Integer(11);
+         improper[2] = new Integer(12);
+         improper[3] = new Integer(13);
+         // improper[4] = new Integer(14);
+         // improper[4] = new Integer(15);
 
-      WriteLine("");
-      WriteLine(improper[0].Princ());
-      WriteLine(improper[1].Princ());
-      WriteLine(improper[2].Princ());
-      WriteLine(improper[3].Princ());
-      WriteLine(improper[4].Princ());
-      WriteLine(improper[5].Princ());
+         WriteLine("");
+         WriteLine(improper[0].Princ());
+         WriteLine(improper[1].Princ());
+         WriteLine(improper[2].Princ());
+         WriteLine(improper[3].Princ());
+         WriteLine(improper[4].Princ());
+         WriteLine(improper[5].Princ());
 
-      WriteLine("");
-      WriteLine(proper[0].Princ());
-      WriteLine(proper[1].Princ());
-      WriteLine(proper[2].Princ());
-      WriteLine(proper[3].Princ());
-      WriteLine(proper[4].Princ());
-      WriteLine(proper[5].Princ());
+         WriteLine("");
+         WriteLine(proper[0].Princ());
+         WriteLine(proper[1].Princ());
+         WriteLine(proper[2].Princ());
+         WriteLine(proper[3].Princ());
+         WriteLine(proper[4].Princ());
+         WriteLine(proper[5].Princ());
 
-      proper[0] = new Integer(10);
-      proper[1] = new Integer(11);
-      proper[2] = new Integer(12);
-      proper[3] = new Integer(13);
-      // proper[4] = new Integer(14);
-      // proper[4] = new Integer(15);
+         proper[0] = new Integer(10);
+         proper[1] = new Integer(11);
+         proper[2] = new Integer(12);
+         proper[3] = new Integer(13);
+         // proper[4] = new Integer(14);
+         // proper[4] = new Integer(15);
 
-      WriteLine("");
-      WriteLine(proper[0].Princ());
-      WriteLine(proper[1].Princ());
-      WriteLine(proper[2].Princ());
-      WriteLine(proper[3].Princ());
-      WriteLine(proper[4].Princ());
-      WriteLine(proper[5].Princ());
+         WriteLine("");
+         WriteLine(proper[0].Princ());
+         WriteLine(proper[1].Princ());
+         WriteLine(proper[2].Princ());
+         WriteLine(proper[3].Princ());
+         WriteLine(proper[4].Princ());
+         WriteLine(proper[5].Princ());
+      }
 
       List<(string name, CoreFunction.FuncT fun, byte minArgs, byte maxArgs, bool special)> coreFuns = [
-         ("eql?", Core.EqlP, 02, 2, true),
-         ("eq?",  Core.EqP, 002, 2, true),
-         ("cons", Core.Cons, 02, 2, true),
-         ("cdr ", Core.Cdr, 001, 1, true),
-         ("car ", Core.Car, 001, 1, true),
+       ("eql?", Core.EqlP, 02, 2, true),
+       ("eq?", Core.EqP, 002, 2, true),
+       ("cons", Core.Cons, 02, 2, true),
+       ("cdr ", Core.Cdr, 001, 1, true),
+       ("car ", Core.Car, 001, 1, true),
       ];
 
       foreach ((string name, CoreFunction.FuncT fun, byte minArgs, byte maxArgs, bool special) in coreFuns)
@@ -266,14 +272,14 @@ class Program
 
       WriteLine(root_env);
 
-      (found, fobj) = root_env.Lookup(Env.LookupMode.Global, Intern("cons"));
+      var (found, fobj) = root_env.Lookup(Env.LookupMode.Global, Intern("cons"));
       var args = Cons(new Integer(1), Cons(new Integer(2), Nil));
       WriteLine(((CoreFunction)fobj).Apply(root_env, args).Princ());
 
       WriteLine(new Rational(2, 4).Princ());
 
       WriteLine(Eval(child_env, "(cons 2 (cons 3 (cons 4 nil)))").Princ());
-    }
+   }
 
    //==============================================================================================================================
 }
