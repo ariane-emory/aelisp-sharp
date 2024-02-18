78e1f1a82821dfda0d063d240b8fa77d14cbc671
Ariane Emory <ariane.emory@protonmail.com>
2024-02-18 10:44:13 -0500

    wip

--- aelisp/src/objects/UserFunctions.cs
+++ aelisp/src/objects/UserFunctions.cs
@@ -16,6 +16,7 @@ static partial class Ae
       public LispObject Parameters { get; }
       public LispObject Body { get; }
       public LispObject Env { get; }
+      public abstract bool Special { get; };
 
       //================================================================================================================
       // Constructor
@@ -44,6 +45,11 @@ static partial class Ae
    public class Lambda : UserFunction
    {
       //================================================================================================================
+      // Public properties
+      //================================================================================================================
+      public override bool Special => false;
+
+      //================================================================================================================
       // Constructor
       //================================================================================================================
       public Lambda(LispObject parameters, LispObject body, LispObject env) : base(parameters, body, env) { }
@@ -53,90 +59,63 @@ static partial class Ae
       //================================================================================================================
       protected override LispObject ImplApply(Env env, LispObject argsList)
       {
-      //    if ((Parameters is Pair pair) &&
-      //        ((argsList.Length < Parameters.Length) ||
-      //         (Parameters.IsProperList && argsList.Length > Paramseters.Length)))
-      //    )
-      //    {
-      //       char* const msg_tmp = free_list_malloc(256);
+         if ((Parameters is Pair pair) &&
+             ((argsList.Length < Parameters.Length) ||
+              (Parameters.IsProperList && argsList.Length > Paramseters.Length)))
+            throw new ArgumentException($"user fun requires {(Parameters.IsProperList ? "exactly" : "at least")} {Parameters.Length} args, but got {argsList.Length}");
 
-      //       snprintf(msg_tmp,
-      //                256,
-      //                "%s:%d: user fun %s requires %s %d arg%s, but got %d",
-      //                __FILE__,
-      //                __LINE__,
-      //                fun_name_part,
-      //                !PROPERP(FUN_PARAMS(fun)) ? "at least" : "exactly",
-      //                LENGTH(FUN_PARAMS(fun)),
-      //                s_or_blank(LENGTH(FUN_PARAMS(fun))),
-      //                LENGTH(args));
+         if (!Special)
+            if (!SPECIAL_FUNP(fun))
+               args = RETURN_IF_ERRORP(EVAL_ARGS(env, args));
 
-      //       char* const msg = free_list_malloc(strlen(msg_tmp) + 1);
-      //       strcpy(msg, msg_tmp);
-      //       free_list_free(msg_tmp);
+         if (log_eval)
+            LOG(args, "applying user fun %s to %d %s arg%s:", fun_name_part, LENGTH(args), (SPECIAL_FUNP(fun) ? "unevaled" : "evaled"), s_or_blank(LENGTH(args)));
 
-      //       ae_obj_t* err_data = NIL;
+         env = NEW_ENV(FUN_ENV(fun), FUN_PARAMS(fun), args);
 
-      //       ae_obj_t * const err = NEW_ERROR(msg);
+         /* if (log_eval) { */
+         /*   LOG(env,           "new env for user fun:"); */
+         /*   LOG(ENV_SYMS(env), "new env's syms:"); */
+         /*   LOG(ENV_VALS(env), "new env's vals:"); */
+         /* } */
 
-      //       PUT_PROP(env, "error-env", err);
-      //       PUT_PROP(args, "error-args", err);
-      //       PUT_PROP(fun, "error-fun", err);
+         INDENT;
 
-      //       RETURN_IF_ERRORP(err);
-      //    }
+         ae_obj_t * const body = FUN_BODY(fun);
 
-      //    if (!SPECIAL_FUNP(fun))
-      //       args = RETURN_IF_ERRORP(EVAL_ARGS(env, args));
+         if (log_eval)
+         {
+            // If FUN_PARAMS(fun) is a blob, we lie to get a plural length:
+            LOG(FUN_PARAMS(fun), "as param%s", s_or_blank(CONSP(FUN_PARAMS(fun)) ? LENGTH(FUN_PARAMS(fun)) : 2));
+            LOG(body, "with body");
+         }
 
-      //    if (log_eval)
-      //       LOG(args, "applying user fun %s to %d %s arg%s:", fun_name_part, LENGTH(args), (SPECIAL_FUNP(fun) ? "unevaled" : "evaled"), s_or_blank(LENGTH(args)));
+         PUT_PROP(fun, "fun", env);
 
-      //    env = NEW_ENV(FUN_ENV(fun), FUN_PARAMS(fun), args);
+         ret = EVAL_AND_RETURN_IF_ERRORP(env, body);
 
-      //    /* if (log_eval) { */
-      //    /*   LOG(env,           "new env for user fun:"); */
-      //    /*   LOG(ENV_SYMS(env), "new env's syms:"); */
-      //    /*   LOG(ENV_VALS(env), "new env's vals:"); */
-      //    /* } */
+         OUTDENT;
 
-      //    INDENT;
+      end:
 
-      //    ae_obj_t * const body = FUN_BODY(fun);
+         if (log_eval)
+         {
+            if (HAS_PROP("last-bound-to", fun))
+            {
+               LOG(ret, "applying user fun '%s' returned %s :%s",
+                   SYM_VAL(GET_PROP("last-bound-to", fun)), a_or_an(GET_TYPE_STR(ret)), GET_TYPE_STR(ret));
+            }
+            else
+            {
+               char* const tmp = SWRITE(fun);
+               LOG(ret, "applying user fun %s returned %s :%s", tmp, a_or_an(GET_TYPE_STR(ret)), GET_TYPE_STR(ret));
+               free(tmp);
+            }
+         }
 
-      //    if (log_eval)
-      //    {
-      //       // If FUN_PARAMS(fun) is a blob, we lie to get a plural length:
-      //       LOG(FUN_PARAMS(fun), "as param%s", s_or_blank(CONSP(FUN_PARAMS(fun)) ? LENGTH(FUN_PARAMS(fun)) : 2));
-      //       LOG(body, "with body");
-      //    }
+         free_list_free(fun_name_part);
 
-      //    PUT_PROP(fun, "fun", env);
-
-      //    ret = EVAL_AND_RETURN_IF_ERRORP(env, body);
-
-      //    OUTDENT;
-
-      // end:
-
-      //    if (log_eval)
-      //    {
-      //       if (HAS_PROP("last-bound-to", fun))
-      //       {
-      //          LOG(ret, "applying user fun '%s' returned %s :%s",
-      //              SYM_VAL(GET_PROP("last-bound-to", fun)), a_or_an(GET_TYPE_STR(ret)), GET_TYPE_STR(ret));
-      //       }
-      //       else
-      //       {
-      //          char* const tmp = SWRITE(fun);
-      //          LOG(ret, "applying user fun %s returned %s :%s", tmp, a_or_an(GET_TYPE_STR(ret)), GET_TYPE_STR(ret));
-      //          free(tmp);
-      //       }
-      //    }
-
-      //    free_list_free(fun_name_part);
-
-      //    JUMP_RETURN_EXIT;
+         JUMP_RETURN_EXIT;
 
          throw new NotImplementedException("Implement this!");
       }
@@ -149,6 +128,11 @@ static partial class Ae
    //===================================================================================================================
    public class Macro : UserFunction
    {
+      //================================================================================================================
+      // Public properties
+      //================================================================================================================
+      public override bool Special => true;
+
       //================================================================================================================
       // Constructor
       //================================================================================================================
