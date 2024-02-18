8e42c39aa7ebbee9228c2173cd7bed5b4b0010e4
Ariane Emory <ariane.emory@protonmail.com>
2024-02-17 23:18:00 -0500

    ...

--- aelisp/src/objects/LispObject.cs
+++ aelisp/src/objects/LispObject.cs
@@ -55,48 +55,6 @@ static partial class Ae
    }
 
    //===================================================================================================================
-   // Symbol class
-   //===================================================================================================================
-   public class Symbol : LispObjectWithStringValue
-   {
-      //================================================================================================================
-      // Constructor
-      //================================================================================================================
-      public Symbol(string value) : base(value)
-      {
-         if (string.IsNullOrEmpty(Value))
-            throw new ArgumentException($"{nameof(Value)} cannot be null or empty", nameof(Value));
-      }
-
-      //================================================================================================================
-      // Public methods
-      //================================================================================================================
-      public override string ToString() => this == Nil ? "Nil" : $"{TypeName}({Value})";
-      public override string Write() => Value;
-
-      public bool IsSpecial => Value[0] == '*' && Value[Value.Length - 1] == '*';
-      public bool IsKeyword => Value[0] == ':';
-      public override bool IsSelfEvaluating => IsKeyword || this == Nil || this == True;
-
-      //================================================================================================================
-      public override LispObject Eval(Env env)
-      {
-         if (IsSelfEvaluating)
-            return this;
-
-         var lookupMode = IsSpecial ? Env.LookupMode.Global : Env.LookupMode.Nearest;
-         var (found, obj) = env.Lookup(lookupMode, this);
-
-         if (!found)
-            throw new ApplicationException($"Unbound symbol '{Write()}!");
-
-         return obj;
-      }
-
-      //================================================================================================================
-   }
-
-   //===================================================================================================================
    // String class
    //===================================================================================================================
    public class String : LispObjectWithStringValue
