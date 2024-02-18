e4814ae6380f227fda135e625c4dd62cbacdf3d2
Ariane Emory <ariane.emory@protonmail.com>
2024-02-18 15:45:04 -0500

    ...

--- aelisp/src/Core.cs
+++ aelisp/src/Core.cs
@@ -479,12 +479,82 @@ static partial class Ae
       public static readonly CoreFun.FuncT LetStar = (env, argsList, argsLength) =>
          LetInternal(env, argsList, argsLength, true);
 
-      //===================================================================================================================
-      private static LispObject LetInternal(Env env, LispObject argsList, int argsLength, bool bindInNewEnv)
-      {
-         var arg0 = ((Pair)argsList).Car;
-         var body = ((Pair)argsList).Cdr;
+      // //===================================================================================================================
+      // public static readonly CoreFun.FuncT LetRev = (env, argsList, argsLength) =>
+      // {
+      //    static ae_obj_t* dummy = NULL;
 
+      //    if (!dummy)
+      //       dummy = KW("DUMMY");
+
+      //    ae_obj_t * const varlist = CAR(args);
+
+      //    REQUIRE(PROPERP(varlist), "varlist must be a proper list");
+      //    // REQUIRE(LENGTH(varlist) > 0, "empty varlist");
+
+      //    FOR_EACH(varlist_item, varlist) {
+      //       REQUIRE(SYMBOLP(varlist_item) || (CONSP(varlist_item) && LENGTH(varlist_item) == 2),
+      //               "varlist items must be symbols or lists with two elements");
+
+      //       ae_obj_t * const sym = SYMBOLP(varlist_item) ? varlist_item : CAR(varlist_item);
+
+      //       REQUIRE((!SPECIAL_SYMP(sym)), "let forms cannot bind special symbols");
+
+      //    }
+
+      //    ae_obj_t * const body    = CDR(args);
+
+      //    REQUIRE(PROPERP(body), "body must be a proper list");
+
+      //    ae_obj_t * const new_env = NEW_ENV(env, NIL, NIL);
+
+      //    FOR_EACH(varlist_item, varlist)
+
+
+      //     ENV_SET_L(new_env, CAR(varlist_item), dummy);  // DUMMY_VALUE can be anything like NIL or a specific uninitialized marker.
+
+      //    FOR_EACH(varlist_item, varlist) {
+      //       if (log_core)
+      //          LOG(varlist_item, "letrec varlist item");
+
+      //       INDENT;
+
+      //       ae_obj_t* val = SYMBOLP(varlist_item)
+      //         ? NIL
+      //         : RETURN_IF_ERRORP(EVAL(new_env, CADR(varlist_item)));
+
+      //       // let only sets the last-bound-to property if it's not already set.
+      //       if ((LAMBDAP(val) || MACROP(val)) && !HAS_PROP("last-bound-to", CAR(varlist_item)))
+      //          PUT_PROP(CAR(varlist_item), "last-bound-to", val);
+
+      //       if (log_core)
+      //       {
+      //          if (SYMBOLP(varlist_item))
+      //             LOG(varlist_item, "binding symbol");
+      //          else
+      //             LOG(CAR(varlist_item), "binding symbol");
+
+      //          LOG(val, "to value");
+      //       }
+
+      //       ENV_SET_L(new_env, SYMBOLP(varlist_item) ? varlist_item : CAR(varlist_item), val);
+
+      //       OUTDENT;
+      //    }
+
+      //    if (log_core)
+      //    {
+      //       LOG(ENV_SYMS(new_env), "new_env syms");
+      //       LOG(ENV_VALS(new_env), "new_env vals");
+      //    }
+
+      //    RETURN(ae_core_progn(new_env, body, LENGTH(body)));
+
+      //    END_DEF_CORE_FUN;
+      // };
+
+      private static void ValidateLetArguments(LispObject arg0, LispObject body)
+      {
          if (!(arg0 is Pair varlist))
             throw new ArgumentException($"varlist must be a list, not {arg0.Princ()}!");
 
@@ -519,7 +589,18 @@ static partial class Ae
 
          if (body.IsNil)
             throw new ArgumentException("body cannot be empty");
+      }
 
+      //===================================================================================================================
+      private static LispObject LetInternal(Env env, LispObject argsList, int argsLength, bool bindInNewEnv)
+      {
+         var arg0 = ((Pair)argsList).Car;
+         var body = ((Pair)argsList).Cdr;
+
+         ValidateLetArguments(arg0, body);
+
+         var varlist = (Pair)arg0;
+         LispObject current = varlist;
          var newEnv = new Env(env, Nil, Nil);
 
          current = varlist;
