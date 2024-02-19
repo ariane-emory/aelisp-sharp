4c8109dc35b2fbbe40cc9c1e4837e85b6a82a2c4
Ariane Emory <ariane.emory@protonmail.com>
2024-02-19 03:42:27 -0500

    ...

---
+++ aelisp/src/core/EqualityPredicateFun.cs
@@ -0,0 +1,44 @@
+using static System.Console;
+using static Ae;
+
+//======================================================================================================================
+static partial class Ae
+{
+   //====================================================================================================================
+   public static partial class Core
+   {
+      //=================================================================================================================
+      private static CoreFun.FuncT EqualityPredicateFun(Func<LispObject, LispObject, bool> pred) =>
+         (env, argsList, argsLength) =>
+         {
+            if (argsList.IsImproperList)
+               throw new ArgumentException("argsList must be a proper list");
+
+            var arg0 = ((Pair)argsList).Car;
+            var current = ((Pair)argsList).Cdr;
+
+            while (current is Pair currentPair)
+            {
+               // WriteLine($"Compare {arg0} to {((Pair)currentPair).Car}.");
+
+               if (!pred(arg0, ((Pair)currentPair).Car))
+                  return Nil;
+
+               current = ((Pair)currentPair).Cdr;
+            }
+
+            return True;
+         };
+
+      //=================================================================================================================
+      public static readonly CoreFun.FuncT EqP =
+        EqualityPredicateFun((o1, o2) => o1 == o2);
+
+      //=================================================================================================================
+      public static readonly CoreFun.FuncT EqlP =
+         EqualityPredicateFun((o1, o2) => o1.Eql(o2));
+
+      //================================================================================================================
+   }
+   //===================================================================================================================
+}
