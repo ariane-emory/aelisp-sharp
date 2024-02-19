0cc166e6e58aee598a49595e641602afb56eef24
Ariane Emory <ariane.emory@protonmail.com>
2024-02-19 04:13:25 -0500

    ...

--- aelisp/src/core/Core.cs
+++ aelisp/src/core/Core.cs
@@ -61,29 +61,6 @@ static partial class Ae
       };
 
       //=================================================================================================================
-      private static CoreFun.FuncT RplacaOrRplacdFun(Action<Pair, LispObject> action) =>
-         (Env env, LispObject argsList, int argsLength) =>
-         {
-            var arg1 = ((Pair)argsList)[0];
-            var arg2 = ((Pair)argsList)[1];
-
-            if (arg1 is not Pair pair)
-               throw new ArgumentException("First argument must be a cons cell!");
-
-            action(pair, arg2);
-
-            return arg2;
-         };
-
-      //=================================================================================================================
-      public static readonly CoreFun.FuncT Rplacd =
-         RplacaOrRplacdFun((pair, arg1) => pair.Cdr = arg1);
-
-      //=================================================================================================================
-      public static readonly CoreFun.FuncT Rplaca =
-         RplacaOrRplacdFun((pair, arg1) => pair.Car = arg1);
-
-      //=================================================================================================================
       private static CoreFun.FuncT CarOrCdrFun(Func<LispObject, LispObject> func) =>
          (Env env, LispObject argsList, int argsLength) =>
          PureUnaryFun((o) =>
