0892cffa4fe0cd513c7aca9bd749bf74e1fa9459
Ariane Emory <ariane.emory@protonmail.com>
2024-02-19 03:27:52 -0500

    ...

---
+++ aelisp/src/core/PureUnaryFun.cs
@@ -0,0 +1,92 @@
+using static System.Console;
+using static Ae;
+
+//======================================================================================================================
+static partial class Ae
+{
+   //====================================================================================================================
+   public static partial class Core
+   {
+      //=================================================================================================================
+      public static readonly CoreFun.FuncT Plus1 =
+         PureUnaryFun(num =>
+            num switch
+            {
+               Integer integer => (LispObject)new Integer(integer.Value + 1),
+               Float floatObj => (LispObject)new Float(floatObj.Value + 1),
+               Rational rational => (LispObject)new Rational(rational.Numerator + rational.Denominator, rational.Denominator),
+               _ => throw new ArgumentException($"Argument must be a number, not {num}!")
+            });
+
+      //=================================================================================================================
+      public static readonly CoreFun.FuncT Minus1 =
+         PureUnaryFun(num =>
+            num switch
+            {
+               Integer integer => (LispObject)new Integer(integer.Value - 1),
+               Float floatObj => (LispObject)new Float(floatObj.Value - 1),
+               Rational rational => (LispObject)new Rational(rational.Numerator - rational.Denominator, rational.Denominator),
+               _ => throw new ArgumentException($"Argument must be a number, not {num}!")
+            });
+
+      //=================================================================================================================
+      private static CoreFun.FuncT PureUnaryFun(Func<LispObject, LispObject> func)
+         => (Env env, LispObject argsList, int argsLength) =>
+         {
+            if (argsList.IsImproperList)
+               throw new ArgumentException("argsList must be a proper list");
+
+            return func(((Pair)argsList)[0]);
+         };
+
+      //=================================================================================================================
+      private static CoreFun.FuncT PureUnaryFun<T1>(Func<T1, LispObject> func)
+       where T1 : LispObject
+       => (Env env, LispObject argsList, int argsLength) =>
+       {
+          if (argsList.IsImproperList)
+             throw new ArgumentException("argsList must be a proper list");
+
+          var arg1 = ((Pair)argsList)[0];
+
+          if (arg1 is T1 typedArg1)
+             return func(typedArg1);
+
+          throw new ArgumentException($"Argument must be of type {typeof(T1).Name}");
+       };
+
+      //=================================================================================================================
+      public static readonly CoreFun.FuncT Id = PureUnaryFun(o => o);
+
+      //=================================================================================================================
+      public static readonly CoreFun.FuncT Quote =
+         PureUnaryFun(o => o);
+
+      //=================================================================================================================
+      public static readonly CoreFun.FuncT Length =
+         PureUnaryFun(o => new Integer(o.Length));
+
+      //=================================================================================================================
+      public static readonly CoreFun.FuncT ObjToString =
+         PureUnaryFun(o => new String(o.Princ()));
+
+      //=================================================================================================================
+      public static readonly CoreFun.FuncT InternString =
+         PureUnaryFun(o =>
+         {
+            if (o is String str)
+               return Intern(str.Value);
+
+            throw new ArgumentException($"Argument must be a string, not {o}!");
+         });
+
+      //===================================================================================================================
+      public static readonly CoreFun.FuncT Type =
+         PureUnaryFun(o => Intern(o is Pair
+                                  ? ":cons"
+                                  : ":" + o.TypeName.ToLower()));
+
+      //================================================================================================================
+   }
+   //===================================================================================================================
+}
