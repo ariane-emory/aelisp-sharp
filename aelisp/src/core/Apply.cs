dddd515c9816dbe9fd1a73ec062ffbd1ed439774
Ariane Emory <ariane.emory@protonmail.com>
2024-02-19 07:20:11 -0500

    ...

--- aelisp/src/core/Apply.cs
+++ aelisp/src/core/Apply.cs
@@ -13,43 +13,45 @@ static partial class Ae
          if (!argsList.IsProperList)
             throw new ArgumentException($"argsList must be a proper list, not {argsList}!");
 
-         WriteLine($"argList: {argsList.Princ()}");
+         WriteLine($"argList:     {argsList.Princ()}");
 
          var arg1 = ((Pair)argsList)[0];
 
          if (!(argsList is Pair argsListPair))
             throw new ArgumentException($"argsList must be a pair, not {argsList}!");
 
-         LispObject current = argsListPair;
-         var stash = Nil;
+         WriteLine($"argListPair: {argsListPair.Princ()}");
 
-         while (current is Pair currentPair && currentPair.Cdr is Pair)
-         {
-            stash = Ae.Cons(currentPair.Car.Eval(env), stash);
-            current = currentPair.Cdr;
-         }
+         // LispObject current = argsListPair;
+         // var stash = Nil;
 
-         WriteLine($"Stash:   {stash.Princ()}");
-         WriteLine($"current: {current.Princ()}");
+         // while (current is Pair currentPair && currentPair.Cdr is Pair)
+         // {
+         //    stash = Ae.Cons(currentPair.Car.Eval(env), stash);
+         //    current = currentPair.Cdr;
+         // }
 
-         var last = ((Pair)current).Car;
-         WriteLine($"last:    {last.Princ()}");
-         var lastIsQuoteForm = last is Pair lastPair && lastPair.Car == Intern("quote");
-         WriteLine(lastIsQuoteForm);
-         var lastTail = (Pair)((Pair)last).Cdr;
-         var newExpr = lastIsQuoteForm ? lastTail.Car : lastTail;
+         // WriteLine($"Stash:   {stash.Princ()}");
+         // WriteLine($"current: {current.Princ()}");
 
-         WriteLine($"newExpr: {newExpr.Princ()}");
+         // var last = ((Pair)current).Car;
+         // WriteLine($"last:    {last.Princ()}");
+         // var lastIsQuoteForm = last is Pair lastPair && lastPair.Car == Intern("quote");
+         // WriteLine(lastIsQuoteForm);
+         // var lastTail = (Pair)((Pair)last).Cdr;
+         // var newExpr = lastIsQuoteForm ? lastTail.Car : lastTail;
 
-         current = stash;
+         // WriteLine($"newExpr: {newExpr.Princ()}");
 
-         while (current is Pair currentPair)
-         {
-            newExpr = Ae.Cons(currentPair.Car, newExpr);
-            current = currentPair.Cdr;
-         }
+         // current = stash;
+
+         // while (current is Pair currentPair)
+         // {
+         //    newExpr = Ae.Cons(currentPair.Car, newExpr);
+         //    current = currentPair.Cdr;
+         // }
          
-         WriteLine($"newExpr after consing: {newExpr.Princ()}");
+         // WriteLine($"newExpr after consing: {newExpr.Princ()}");
 
          // return newExpr.Eval(env);
          return Nil;
