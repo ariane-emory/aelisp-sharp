4ec230671f89246ff9c0b6d0c4d4803a0b082ea9
Ariane Emory <ariane.emory@protonmail.com>
2024-02-19 05:48:35 -0500

    ...

--- aelisp/src/core/functions.cs
+++ aelisp/src/core/functions.cs
@@ -12,7 +12,15 @@ static partial class Ae
            obj is Symbol symbol && (!(symbol.IsSpecial || symbol.IsKeyword || symbol == True || symbol == Nil));
 
       //=================================================================================================================
-      public static readonly CoreFun.FuncT Lambda = (env, argsList) =>
+      public static LispObject NewLambda(Env env, LispObject argsList)
+          => NewFunction<Lambda>(env, argsList);
+
+      //=================================================================================================================
+      public static LispObject NewMacro(Env env, LispObject argsList)
+          => NewFunction<Macro>(env, argsList);
+
+      //=================================================================================================================
+      public static LispObject NewFunction<T>(Env env, LispObject argsList)
       {
          if (argsList.IsImproperList)
             throw new ArgumentException($"argsList must be a proper list, not {argsList}!");
@@ -43,41 +51,7 @@ static partial class Ae
             throw new ArgumentException($"Can't use {currentParam.Princ()} as a parameter!");
 
          return new Lambda(env, lambdaList, body);
-      };
-
-      //=================================================================================================================
-      public static readonly CoreFun.FuncT Macro = (env, argsList) =>
-      {
-         if (argsList.IsImproperList)
-            throw new ArgumentException($"argsList must be a proper list, not {argsList}!");
-
-         var lambdaList = ((Pair)argsList)[0];
-         var body = ((Pair)argsList)[1];
-
-         if (!(lambdaList.IsList || IsPermittedParamSymbol(lambdaList)))
-            throw new ArgumentException($"Lambda list must be a list or a symbol, not {lambdaList.Princ()}!");
-
-         if (lambdaList is Symbol symbol && (symbol.IsSpecial || symbol.IsKeyword || symbol == True))
-            throw new ArgumentException($"Can't use {symbol.Princ()} as a parameter!");
-
-         if (!body.IsProperList)
-            throw new ArgumentException($"body must be a proper list, not {body.Princ()}");
-
-         var currentParam = lambdaList;
-
-         while (currentParam is Pair currentParamPair)
-         {
-            if (!IsPermittedParamSymbol(currentParamPair.Car))
-               throw new ArgumentException($"Can't use {currentParamPair.Car.Princ()} as a parameter!");
-
-            currentParam = currentParamPair.Cdr;
-         }
-
-         if (currentParam != Nil && !IsPermittedParamSymbol(currentParam))
-            throw new ArgumentException($"Can't use {currentParam.Princ()} as a parameter!");
-
-         return new Macro(env, lambdaList, body);
-      };
+      }
 
       //================================================================================================================
    }
