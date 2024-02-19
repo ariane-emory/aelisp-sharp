504c987c61cb33d30decffb030b2f383196e7728
Ariane Emory <ariane.emory@protonmail.com>
2024-02-19 03:38:25 -0500

    ...

--- aelisp/src/core/AccessorFun.cs
+++ aelisp/src/core/AccessorFun.cs
@@ -41,34 +41,6 @@ static partial class Ae
       public static readonly CoreFun.FuncT SymbolName =
          AccessorFun<Symbol, string>(sym => sym.Value, s => new String(s));
 
-      //=================================================================================================================
-      private static CoreFun.FuncT AccessorFunWithZeroArgsCase<ThisLispObjT, FieldT>(Func<ThisLispObjT, FieldT> getField,
-                                                                                     Func<FieldT, LispObject> construct,
-                                                                                     Func<LispObject> thunk) =>
-         (Env env, LispObject argsList, int argsLength) =>
-          (argsList.Length == 0)
-               ? thunk()
-               : AccessorFun(getField, construct)(env, argsList, argsLength);
-
-
-      //=================================================================================================================
-      public static readonly CoreFun.FuncT EnvOrFunEnv =
-         AccessorFunWithZeroArgsCase<UserFunction, LispObject>(fun => fun.Environment,
-                                                               n => n,
-                                                               () => Root);
-
-      //=================================================================================================================
-      public static readonly CoreFun.FuncT EnvSymbols =
-         AccessorFunWithZeroArgsCase<Env, LispObject>(env => env.Symbols,
-                                                      n => n,
-                                                      () => Root.Symbols);
-
-      //=================================================================================================================
-      public static readonly CoreFun.FuncT EnvValues =
-         AccessorFunWithZeroArgsCase<Env, LispObject>(env => env.Values,
-                                                      n => n,
-                                                      () => Root.Values);
-
       //================================================================================================================
    }
    //===================================================================================================================
---
+++ aelisp/src/core/AccessorFunWithZeroArgsCase.cs
@@ -0,0 +1,41 @@
+using static System.Console;
+using static Ae;
+
+//======================================================================================================================
+static partial class Ae
+{
+   //====================================================================================================================
+   public static partial class Core
+   {
+      //=================================================================================================================
+      private static CoreFun.FuncT AccessorFunWithZeroArgsCase<ThisLispObjT, FieldT>(Func<ThisLispObjT, FieldT> getField,
+                                                                                     Func<FieldT, LispObject> construct,
+                                                                                     Func<LispObject> thunk) =>
+         (Env env, LispObject argsList, int argsLength) =>
+          (argsList.Length == 0)
+               ? thunk()
+               : AccessorFun(getField, construct)(env, argsList, argsLength);
+
+
+      //=================================================================================================================
+      public static readonly CoreFun.FuncT EnvOrFunEnv =
+         AccessorFunWithZeroArgsCase<UserFunction, LispObject>(fun => fun.Environment,
+                                                               n => n,
+                                                               () => Root);
+
+      //=================================================================================================================
+      public static readonly CoreFun.FuncT EnvSymbols =
+         AccessorFunWithZeroArgsCase<Env, LispObject>(env => env.Symbols,
+                                                      n => n,
+                                                      () => Root.Symbols);
+
+      //=================================================================================================================
+      public static readonly CoreFun.FuncT EnvValues =
+         AccessorFunWithZeroArgsCase<Env, LispObject>(env => env.Values,
+                                                      n => n,
+                                                      () => Root.Values);
+
+      //================================================================================================================
+   }
+   //===================================================================================================================
+}
