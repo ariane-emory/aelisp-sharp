75b32c47024d39fcbd72b232198e931962207a76
Ariane Emory <ariane.emory@protonmail.com>
2024-02-19 06:37:15 -0500

    ...

--- aelisp/src/core/Apply.cs
+++ aelisp/src/core/Apply.cs
@@ -7,73 +7,31 @@ static partial class Ae
    //====================================================================================================================
    public static partial class Core
    {
-      //=================================================================================================================
-      public static readonly CoreFun.FuncT Apply = (env, argsList) =>
+      //================================================================================================================
+      public static LispObject Apply(Env env, LispObject argsList)
       {
-         if (argsList is not Pair argsPair)
-            throw new ArgumentException("apply expects at least one argument");
+         if (!argsList.IsProperList)
+            throw new ArgumentException($"argsList must be a proper list, not {argsList}!");
 
-         var func = argsPair.Car;
-         var args = argsPair.Cdr;
+         var arg1 = ((Pair)argsList)[0];
 
-         if (args.IsImproperList || args.IsNil)
-            throw new ArgumentException("apply expects the last argument to be a proper list");
+         if (!(argsList is Pair argsListPair))
+            throw new ArgumentException($"argsList must be a pair, not {argsList}!");
 
-         // Build new expression
-         var newExpr = new List<LispObject> { func };
-         while (args is Pair argsPairRest && argsPairRest.Cdr is Pair)
+         LispObject current = argsListPair;
+         var stash = Nil;
+         
+         while (current is Pair currentPair && currentPair.Cdr is Pair)
          {
-            var arg = argsPairRest.Car;
-            var evaluatedArg = IsQuoteForm(arg) ? Unquote(arg) : arg.Eval(env);
-            newExpr.Add(Requote(evaluatedArg));
-            args = argsPairRest.Cdr;
+            stash = Ae.Cons(currentPair.Car, stash);
+            current = currentPair.Cdr;
          }
 
-         // Handle last argument, which should be a list
-         var lastArg = args is Pair lastPairArg ? lastPairArg.Car : args;
-         var lastList = IsQuoteForm(lastArg) ? Unquote(lastArg) : lastArg.Eval(env);
-         if (lastList is not Pair && lastList != Nil)
-            throw new ArgumentException("apply requires the last argument to be a proper list");
+         WriteLine($"Stash: {stash.Princ()}");
+         WriteLine($"current: {current.Princ()}");
 
-         foreach (var item in ToList(lastList))
-         {
-            newExpr.Add(item);
-         }
-
-         // Evaluate the new expression
-         var newExprList = ListToPair(newExpr);
-
-         return newExprList.Eval(env);
-      };
-
-      private static bool IsQuoteForm(LispObject obj) =>
-          obj is Pair pair && pair.Car.Equals(Intern("quote")) && pair.Cdr is Pair;
-
-      private static LispObject Unquote(LispObject quoted) =>
-          ((Pair)((Pair)quoted).Cdr).Car;
-
-      private static LispObject Requote(LispObject obj) =>
-          Ae.Cons(Intern("quote"), Ae.Cons(obj, Nil));
-
-      private static IEnumerable<LispObject> ToList(LispObject list)
-      {
-         while (list is Pair pair)
-         {
-            yield return pair.Car;
-            list = pair.Cdr;
-         }
-      }
-
-      private static Pair ListToPair(IEnumerable<LispObject> list)
-      {
-         // Start with an empty pair (Nil . Nil) as the seed for aggregation.
-         var seed = new Pair(Nil, Nil);
-         // Aggregate the list into a Pair structure.
-         var result = list.Aggregate(seed, (current, item) => new Pair(item, current));
-
-         // Since we're always working with Pair instances, the result is guaranteed to be a Pair.
-         return result;
-      }
+         return Nil;
+        }
 
       //================================================================================================================
    }
