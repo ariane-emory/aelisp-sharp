c8f36862e53e877bac5bb2fe33c83b299b840149
Ariane Emory <ariane.emory@protonmail.com>
2024-02-19 09:48:56 -0500

    ...

--- aelisp/src/core/consoleIO.cs
+++ aelisp/src/core/consoleIO.cs
@@ -9,7 +9,7 @@ static partial class Ae
    public static partial class Core
    {
       //================================================================================================================
-      public static LispObject WriteObj(Env env, LispObject argsList)
+      public static LispObject OutputFun(Env env, LispObject argsList, Func<LispObject, string> fun)
       {
          if (argsList.IsImproperList)
             throw new ArgumentException($"argsList must be a proper list, not {argsList}!");
@@ -20,8 +20,8 @@ static partial class Ae
          while (current is Pair pair)
          {
             var elem = pair.Car;
-            var str = elem.Princ();
-
+            var str = fun(elem);
+            
             Write(str);
             written += str.Length;
 
@@ -29,6 +29,12 @@ static partial class Ae
          }
 
          return new Integer(written);
+      }
+
+      //================================================================================================================
+      public static LispObject WriteObj(Env env, LispObject argsList)
+      {
+         return OutputFun(env, argsList, o => o.Princ());
       }
 
       //================================================================================================================
