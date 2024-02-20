6801ee977dacf091f4f5222790b847aca171ab40
Ariane Emory <ariane.emory@protonmail.com>
2024-02-20 18:36:29 -0500

    ...

--- aelisp/src/Ae.cs
+++ aelisp/src/Ae.cs
@@ -14,8 +14,8 @@ static partial class Ae
    // Static fields
    //====================================================================================================================
    public static readonly LispObject Nil = (LispObject)new Symbol("nil");
-   public static readonly LispObject True = Intern("t");
    private static LispObject SymbolsList = Nil;
+   public static readonly LispObject True = Intern("t");
 
    //====================================================================================================================
    // Static properties
