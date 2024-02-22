56b280fa4ca1952b465ce56b2d3030317de83836
Ariane Emory <ariane.emory@protonmail.com>
2024-02-21 23:44:43 -0500

    ...

--- aelisp/src/data/plistMethods.cs
+++ aelisp/src/data/plistMethods.cs
@@ -7,8 +7,18 @@ static partial class Ae
   //====================================================================================================================
   // plist related methods
   //====================================================================================================================
-   public static bool PlistContains(LispObject Plist, LispObject key)
+   public static bool PlistContains(LispObject plist, LispObject key)
    {
+      if (!plist.IsList)
+         throw new ArgumentException($"plist must be a list, not {plist}!");
+
+      is (!(plist is Pair plistHeadPair)) // plist must be Nil.
+         return false;
+
+      if (!(plistHeadPair.Length % 2 == 0))
+         throw new ArgumentException($"plist must have an even number of elements, not {plistHeadPair.Length}!");
+
+      
       return false;
    }
    
